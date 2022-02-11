99c1d0a55cf21b564f8b210ccd203fe730c24a41
frankkk1013 <59925828+frankkk1013@users.noreply.github.com>
2022-02-11 09:56:48 +0100

    Initial Commit

---
+++ MentisMeditate/MentisMeditateApp.swift
@@ -0,0 +1,17 @@
+//
+//  MentisMeditateApp.swift
+//  MentisMeditate
+//
+//  Created by Francesco Iaccarino on 11/02/22.
+//
+
+import SwiftUI
+
+@main
+struct MentisMeditateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
