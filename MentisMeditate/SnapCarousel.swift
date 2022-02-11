ce197a1a255dc716c6818903a3373fc23f120d47
frankkk1013 <59925828+frankkk1013@users.noreply.github.com>
2022-02-11 10:27:21 +0100

    carosello

---
+++ MentisMeditate/SnapCarousel.swift
@@ -0,0 +1,20 @@
+//
+//  SnapCarousel.swift
+//  MentisMeditate
+//
+//  Created by Francesco Iaccarino on 11/02/22.
+//
+
+import SwiftUI
+
+struct SnapCarousel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SnapCarousel_Previews: PreviewProvider {
+    static var previews: some View {
+        SnapCarousel()
+    }
+}
